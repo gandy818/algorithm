142c94405651104026445a77d112b649c517497f
gandy818 <gandy818@gmail.com>
2023-04-01 20:37:25 +0900

    콜라츠 추측

---
+++ programmers/level1/콜라츠 추측.js
@@ -0,0 +1,14 @@
+function solution(num) {
+  let count = 0;
+  while (num > 1) {
+    var answer = 0;
+    if (num % 2 === 0) {
+      num = num / 2;
+    } else num = num * 3 + 1;
+    count++;
+  }
+  if (count > 500) {
+    answer = -1;
+  } else answer = count;
+  return answer;
+}
